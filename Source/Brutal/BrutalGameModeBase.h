09ac1b32adaa73f47561a855299309df89e1ab55
Austin Harrell <ajharrell2014@gmail.com>
2023-03-08 19:13:02 -0800

    added in idea folder to gitignore

---
+++ Source/Brutal/BrutalGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BrutalGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BRUTAL_API ABrutalGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
